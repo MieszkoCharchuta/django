b60de11318cfbd8d1c9688ca7331022a0ba439c2
Mikołaj Mieszko Charchuta <96045564+MieszkoCharchuta@users.noreply.github.com>
2024-10-28 12:37:06 +0100

    Updated the weather app (just copied AZ's, to simplyfy the database) and everything works

--- weather/models.py
+++ weather/models.py
@@ -1,8 +1,3 @@
 from django.db import models
-from django.utils import timezone
 
-class WeatherCondition(models.Model):
-    area = models.CharField(max_length=10, default="Unknown")
-    observed_at = models.DateTimeField(default=timezone.now)
-    temperature = models.CharField(max_length=10, default="Unknown")
-    pressure = models.CharField(max_length=10, default="Unknown")
+# Create your models here.
--- weather/views.py
+++ weather/views.py
@@ -1,36 +1,24 @@
-from lib2to3.fixes.fix_input import context
-
-import requests
-from django.shortcuts import render
-from .models import WeatherCondition
 from datetime import datetime
+import requests
 
-SOURCES = [
-	{'id': 'London', 'name': 'London'},
-    {'id': 'Milan', 'name': 'Milan'},
-    {'id': 'Berlin', 'name': 'Berlin'},
-    {'id': 'Paris', 'name': 'Paris'},
-    {'id': 'Madrid', 'name': 'Madrid'},
-]
+from django.shortcuts import render
 
-def index_view(request, source='Poznan'):
-    url = f"https://wttr.in/{source}?format=j1"
-    response = requests.get(url)
+
+CITIES = ['Poznan', 'London', 'Berlin', 'New York']
+
+
+def index_view(request, city='Poznan'):
+    URL = f'https://wttr.in/{city}?format=j1'
+    response = requests.get(URL)
     data = response.json()
-
-    # Extract the required fields
-    current = data['current_condition'][0]
-    weather_condition = WeatherCondition(
-        area=data['nearest_area'][0]['areaName'][0]['value'],
-        observed_at=datetime.strptime(current["localObsDateTime"], '%Y-%m-%d %I:%M %p'),
-        temperature=current["temp_C"],
-        pressure=current["pressure"]
-    )
-    # Standardized context setup (because I could not get it to work with bootstrapped base.html extensions
     context = {
-        'weather': weather_condition,  # Makes `weather` variable available in the template
-        'sources': SOURCES,
-        'active_source': source,
+        'cities': CITIES,
+        'data': data,
+        'active_city': city,
     }
+    date_str = data['current_condition'][0]['localObsDateTime']
+    context['date'] = datetime.strptime(
+        date_str,
+        "%Y-%m-%d %H:%M %p"
+    )
     return render(request, 'weather/index.html', context)
-
