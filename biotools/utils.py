e8b3f68427b1507003f61ab4cd8789360a83ba6a
Mikołaj Mieszko Charchuta <96045564+MieszkoCharchuta@users.noreply.github.com>
2024-11-06 10:52:20 +0100

    Added percentages

--- biotools/utils.py
+++ biotools/utils.py
@@ -1,8 +1,17 @@
 def count_words(seq, word_size):
     d = {}
+    # Count occurrences of each word of length `word_size`
     for i in range(0, len(seq) - word_size + 1):
         word = seq[i : i + word_size]
         if word not in d:
-            d[word] = 0
-        d[word] += 1
+            d[word] = {"count": 0, "percentage": 0}
+        d[word]["count"] += 1
+
+    # Calculate the total number of occurrences
+    total_occurrences = sum(word_data["count"] for word_data in d.values())
+
+    # Calculate and store the percentage for each word
+    for word in d:
+        d[word]["percentage"] = (d[word]["count"] / total_occurrences) * 100
+
     return d
